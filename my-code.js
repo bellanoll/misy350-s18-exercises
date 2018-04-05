212e61dd9e5b738a53c03e8e12c7dc1b316d164b
Isabella Noll <36270012+bellanoll@users.noreply.github.com>
2018-04-05 10:35:52 -0400

    Add files via upload

---
+++ my-code.js
@@ -0,0 +1,14 @@
+blist = ["end of the semester", "no research", "ready for summer"];
+console.log(blist.length);
+blist.push("get a beer")
+console.log(blist.length);
+
+console.log(blist.length);
+console.log(blist);
+
+/* for loop
+blabla
+*/
+for (i = 0; i < blist.length; i++) {
+  console.log(blist[i]);
+}
