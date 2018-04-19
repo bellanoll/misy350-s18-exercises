7623adfdcb7d8c68d23eba1978040fcd182a2848
Isabella Noll <36270012+bellanoll@users.noreply.github.com>
2018-04-19 10:24:05 -0400

    Add files via upload

---
+++ pythin-basics.py
@@ -0,0 +1,9 @@
+# this is my first python code
+greeting = "Hello"
+name = "Bella"
+print greeting # this print out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + "" + name + "!"
+print "%s %s! How are you doing, today" % (greeting,name)
